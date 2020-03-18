ce88da6a2352a07b6d4b3d3e34204b9f79641d7a
cderek <pealstyle@gmail.com>
2020-03-19 00:31:26 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Getting started
+
+```sh
+yarn install
+yarn start
+```
+
+## Commands
+
+```sh
+yarn                # install dependencies and build the application
+yarn start          # start the node server
---
+++ src/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const server = require('./server');
+
+const { PORT } = require('./config');
+const { logger } = require('./utils/logger');
+
+server.listen(PORT, () => {
+    logger.info(`Application available at: http://localhost:${PORT}`);
+});
+
